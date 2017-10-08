141a56d4513b7bca72dfa497c7277acaa60ab32d
Dima-f <dafw@live.ru>
2017-10-08 12:38:00 +0300

    webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ const config = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   }
 };
 
